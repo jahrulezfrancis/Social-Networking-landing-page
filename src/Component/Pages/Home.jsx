d6ac8365290b0c760a696941259671cb0b0bb56c
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-05 21:49:56 +0100

    added header section component

--- src/Component/Pages/Home.jsx
+++ src/Component/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/react";
+import { HeaderSec } from "../Header/HeaderSection";
 import { NavigatonBar } from "../Header/Navigation";
 
 export default function HomePage() {
     return (
         <Box>
             <NavigatonBar />
+            <HeaderSec />
         </Box>
 
     )
