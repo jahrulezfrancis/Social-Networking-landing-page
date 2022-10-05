26a61ef53924091c071545ecd0af24166fad7caa
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-05 20:02:01 +0100

    Added route to homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { Box } from '@chakra-ui/react';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from './Component/Pages/Home';
 
 
 function App() {
   return (
-   <Box>Heading test</Box>
+    <React.Fragment>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='home' element={<HomePage />} />
+      </Routes>
+    </React.Fragment>
   );
 }
 
