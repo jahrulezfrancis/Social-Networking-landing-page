0e36bf36343e6f682742a548aca7fc882b74c75e
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-05 23:03:09 +0100

    added about component

--- src/Component/Pages/Home.jsx
+++ src/Component/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import { HeaderSec } from "../Header/HeaderSection";
 import { NavigatonBar } from "../Header/Navigation";
+import { AboutNetboks } from "../Sections/Home";
 
 export default function HomePage() {
     return (
         <Box>
             <NavigatonBar />
             <HeaderSec />
+            <AboutNetboks />
         </Box>
 
     )
