1386adfdef575c9f92f8489bfd7ffaade51d6146
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-05 22:00:10 +0100

    added backgroudn and fixed display on mobile

--- src/Component/Header/HeaderSection.jsx
+++ src/Component/Header/HeaderSection.jsx
@@ -1,9 +1,10 @@
-import { Box, Button, Heading, HStack, Text, Image, VStack, Stack } from "@chakra-ui/react";
+import { Box, Button, Heading, HStack, Text, Image, VStack, Stack, useMediaQuery } from "@chakra-ui/react";
 import People from "../Images/Iamge.png"
 
 export function HeaderSec() {
+    const [mobileDevice] = useMediaQuery('(max-width: 100px)')
     return (
-        <Box p='1em'>
+        <Box m={mobileDevice ? '0em' : '1em'} p='1em' background='linear-gradient(311.76deg, #D4E7FE -15.24%, #FFFFFF 78.85%)'>
             <HStack justify='space-around'>
                 <VStack align='start' gap='2em'>
                     <Button color='#217BF4'>Netbook community</Button>
