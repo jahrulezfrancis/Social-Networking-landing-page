8a41cca0eddb0317ca307f1b141319a77e62a4cf
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-06 14:27:11 +0100

    initial commit

---
+++ src/Component/Sections/Features.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@chakra-ui/react"
+
+function FeaturesTemp() {
+    return (
+        <Box>
+            Feature check one two
+        </Box>
+    )
+}
+
+export function Features() {
+    return (
+        <Box>
+            <FeaturesTemp />
+        </Box>
+    )
+}
