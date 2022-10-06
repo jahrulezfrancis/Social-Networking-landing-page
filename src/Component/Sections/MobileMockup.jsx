3630ae8683db14c7c8bca27ada98f53ad7f2cd1f
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-06 22:54:05 +0100

    Initial commit

---
+++ src/Component/Sections/MobileMockup.jsx
@@ -0,0 +1,24 @@
+import { Box, HStack, VStack, Text, Heading, } from "@chakra-ui/react";
+
+
+
+export function MobileMockUp() {
+    return (
+        <Box>
+            <HStack>
+                <VStack>
+                    <Text>Get Our Aplication</Text>
+                    <Heading>
+                        You Can Easily Find
+                        This App…!
+                    </Heading>
+                    <Text>
+                        I say chap that’s suing lavatory chip shop <br />
+                        gosh off his smashing boot are you taking <br />
+                        the piss posh loo brilliant.
+                    </Text>
+                </VStack>
+            </HStack>
+        </Box>
+    )
+}
