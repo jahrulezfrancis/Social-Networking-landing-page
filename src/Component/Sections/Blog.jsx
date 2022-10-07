c6613db6f9242f8f91c998ab25d70672ac2cdde6
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-07 11:05:21 +0100

    initial commit

---
+++ src/Component/Sections/Blog.jsx
@@ -0,0 +1,44 @@
+import { Box, Text, VStack, Heading, HStack, Image, Progress, Button, Stack } from "@chakra-ui/react";
+import PcImage from "../Images/pc-with-stats.png"
+import GirlDad from "../Images/child-n-dad.png"
+import PinImage from "../Images/office-pin.png"
+import { FcRight } from "react-icons/fc"
+
+
+function BlogPostTemplate(props) {
+    return (
+        <Box>
+            <VStack align='start'>
+                <Image src={props.Image} alt='featured image' />
+                <HStack>
+                    <hr style={{ width: '50px', fontSize: '20px', backgroundColor: '#095484' }} />
+                    <Progress value={20} size='xs' colorScheme='pink' />
+                    <Heading fontFamily='Inter' lineHeight='32px' fontSize='1.2em' color='#2F2C4A'>{props.HeadingText1} <br /> {props.HeadingText2}</Heading>
+                </HStack>
+                <Button color={props.textColor} pl='4em' textAlign='end' background='transparent' _hover={{ background: 'none' }}
+                    alignItems='center' rightIcon={<FcRight color={props.iconColor} />}>
+                    Continue Reading
+                </Button>
+            </VStack>
+        </Box>
+    )
+}
+
+
+export function BlogSection() {
+    return (
+        <Box mt='5em'>
+            <VStack gap='2em'>
+                <Stack align='center'>
+                    <Text>Get Our Application</Text>
+                    <Heading>Latest News</Heading>
+                </Stack>
+                <HStack gap='2em'>
+                    <BlogPostTemplate textColor='#656464' iconColor='#656464' Image={PcImage} HeadingText1='It Does Not Matter Hows' HeadingText2='Slowly go as Long' />
+                    <BlogPostTemplate textColor='#217BF4' iconColor='#217BF4' Image={GirlDad} HeadingText1='Netbook Network Added' HeadingText2='New Photo Filter' />
+                    <BlogPostTemplate textColor='#656464' iconColor='#656464' Image={PinImage} HeadingText1='We Optimised Netbooks' HeadingText2='Better Navigation' />
+                </HStack>
+            </VStack>
+        </Box>
+    )
+}
