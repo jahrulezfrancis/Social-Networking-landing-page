e187c46c16b315806048bcafe0b8b6086e68bcf8
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-08 06:59:37 +0100

    fixed responsiveness

--- src/Component/Header/HeaderSection.jsx
+++ src/Component/Header/HeaderSection.jsx
@@ -1,15 +1,18 @@
-import { Box, Button, Heading, HStack, Text, Image, VStack, Stack, useMediaQuery } from "@chakra-ui/react";
+import { Box, Button, Heading, HStack, Text, Image, VStack, Stack, useMediaQuery, Flex } from "@chakra-ui/react";
 import People from "../Images/Iamge.png"
 
 export function HeaderSec() {
-    const [mobileDevice] = useMediaQuery('(max-width: 100px)')
+    const [mobileDevice] = useMediaQuery('(max-width: 1000px)')
     return (
-        <Box m={mobileDevice ? '0em' : '1em'} p='1em' background='linear-gradient(311.76deg, #D4E7FE -15.24%, #FFFFFF 78.85%)'>
-            <HStack justify='space-around'>
+        <Box m={mobileDevice ? '6em 0em' : '3em 1em'} p='1em' background='linear-gradient(311.76deg, #D4E7FE -15.24%, #FFFFFF 78.85%)'>
+            <Flex justify='space-around' direction={mobileDevice ? 'column' : 'row'}>
                 <VStack align='start' gap='2em'>
                     <Button color='#217BF4'>Netbook community</Button>
+                    <Stack display={mobileDevice ? 'block' : 'none'} justify='center' align='center'>
+                        <Image src={People} alt="" />
+                    </Stack>
                     <Stack>
-                        <Heading color='#0A093D'>
+                        <Heading  color='#0A093D'>
                             Your Solutions For <br />
                             Community!
                         </Heading>
@@ -23,10 +26,10 @@ export function HeaderSec() {
                         <Button border='2px solid #217BF4' variant='outline' fontSize='.8em' color='#217BF4'>Invite Friend</Button>
                     </HStack>
                 </VStack>
-                <Box>
+                <Box display={mobileDevice ? 'none' : 'block'}>
                     <Image src={People} alt="" />
                 </Box>
-            </HStack>
+            </Flex>
         </Box>
     )
 }
