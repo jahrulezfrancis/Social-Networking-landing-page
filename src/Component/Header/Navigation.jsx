59efc910c1760d3543686559f7b79c527591d330
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-08 08:52:40 +0100

    created mobile menu component

--- src/Component/Header/Navigation.jsx
+++ src/Component/Header/Navigation.jsx
@@ -1,7 +1,11 @@
-import { Box, Flex, Image, List, ListItem, InputGroup, InputLeftElement, Input, HStack, Button, useMediaQuery } from "@chakra-ui/react";
+import { Box, Flex, Image, List, ListItem, InputGroup, InputLeftElement, Input, HStack, Button, useMediaQuery, Stack, Spacer } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 import HeaderLogo from "../Images/logo.png"
 import { MdSearch } from "react-icons/md"
+import { BiChevronDown } from "react-icons/bi"
+import { Menu, MenuButton, MenuList, MenuItem, IconButton } from '@chakra-ui/react'
+import { CgMenuGridO } from "react-icons/cg"
+
 
 export function NavigatonBar() {
     const [mobileDevice] = useMediaQuery('(max-width: 1000px)')
@@ -17,7 +21,21 @@ export function NavigatonBar() {
                             <NavLink>Home</NavLink>
                         </ListItem>
                         <ListItem>
-                            <NavLink>Community</NavLink>
+                            <NavLink>
+                                <Menu>
+                                    {({ isOpen }) => (
+                                        <>
+                                            <MenuButton isActive={isOpen} style={{ background: 'transparent' }} as={Button} rightIcon={<BiChevronDown />}>
+                                                Community
+                                            </MenuButton>
+                                            <MenuList>
+                                                <MenuItem>Forum</MenuItem>
+                                                <MenuItem>Friends</MenuItem>
+                                            </MenuList>
+                                        </>
+                                    )}
+                                </Menu>
+                            </NavLink>
                         </ListItem>
                         <ListItem>
                             <NavLink>Blog</NavLink>
@@ -38,6 +56,60 @@ export function NavigatonBar() {
                     </Flex>
                 </HStack>
             </List>
+        </Box>
+    )
+}
+
+
+export function MobileNav() {
+    const [mobileDevice] = useMediaQuery('(max-width: 1000px)')
+
+    return (
+        <Box display={mobileDevice ? 'block' : 'none'} zIndex='10' background='white' p='1em' position='fixed' top='0' right='0' left='0'>
+            <HStack >
+                <Stack>
+                    <Image src={HeaderLogo} alt='header logo' />
+                </Stack>
+                <Spacer />
+                <Menu>
+                    <MenuButton
+                        bg='transparent'
+                        as={IconButton}
+                        aria-label='Options'
+                        icon={<CgMenuGridO />}
+                        fontSize='3em'
+                        variant='none'
+                    />
+                    <MenuList>
+                        <MenuItem>
+                            <NavLink>Events</NavLink>
+                        </MenuItem>
+                        <MenuItem >
+                            <NavLink>
+                                <Menu>
+                                    {({ isOpen }) => (
+                                        <>
+                                            <MenuButton isActive={isOpen} style={{ background: 'transparent' }} as={Button} rightIcon={<BiChevronDown />}>
+                                                Community
+                                            </MenuButton>
+                                            <MenuList>
+                                                <MenuItem>Forum</MenuItem>
+                                                <MenuItem>Friends</MenuItem>
+                                            </MenuList>
+                                        </>
+                                    )}
+                                </Menu>
+                            </NavLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <NavLink>Events</NavLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <NavLink>Events</NavLink>
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+            </HStack>
         </Box>
     )
 }
