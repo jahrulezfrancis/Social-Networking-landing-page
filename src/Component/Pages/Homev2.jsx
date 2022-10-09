12ea5ac97798aafa9a719a17ca11fcf1128484d9
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-09 23:23:58 +0100

    updated header section

--- src/Component/Pages/Homev2.jsx
+++ src/Component/Pages/Homev2.jsx
@@ -1,9 +1,22 @@
 import { Box } from "@chakra-ui/react";
+import { NavigatonBarV2 } from "../Header/Homev2/Navigation";
+
+
+function PageV2Header() {
+    return (
+        <Box style={{
+           background:  "linear-gradient(#8b00ff, #e91d1d, #8b00ff)"
+        }} h='10em'>
+            <NavigatonBarV2 />
+        </Box>
+    )
+}
+
 
 export function HomePageV2() {
     return (
         <Box>
-            
+            <PageV2Header />
         </Box>
     )
 }
