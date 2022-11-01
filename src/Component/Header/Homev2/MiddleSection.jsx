5fb57fc79a9cdda3d64158b76ab83a8fdfee9bfe
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-01 21:15:09 +0100

    updated Middle section component

---
+++ src/Component/Header/Homev2/MiddleSection.jsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export function MiddleSection() {
+    return (
+        <Box>
+            <Heading>Middle section component
+            </Heading>
+        </Box>
+    )
+}
