26afed91fcb27751d8ae7f46e0d8fb2209378200
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-02 20:48:03 +0100

    added card images to component

--- src/Component/Header/Homev2/MiddleSection.jsx
+++ src/Component/Header/Homev2/MiddleSection.jsx
@@ -1,5 +1,9 @@
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, HStack, Image, Stack, Text } from "@chakra-ui/react";
 import { BiRightArrowAlt } from "react-icons/bi";
+import Card1 from "../../Images/card1.png"
+import Card2 from "../../Images/card3.png"
+import Card3 from "../../Images/Card2.png"
+
 
 export function MiddleSection() {
     return (
@@ -13,6 +17,17 @@ export function MiddleSection() {
                 profiles to find results 100x faster.
             </Text>
             <Button rightIcon={<BiRightArrowAlt />} w='204px' borderRadius='24px' textAlign='center' fontSize='15px' bg='#231A36' color='white'>Request Demo</Button>
+            <Stack>
+
+                <HStack>
+                    <Image src={Card1} alt='' />
+                    <Image src={Card2} alt='' />
+                </HStack>
+                <HStack>
+                    <Image src={Card3} alt='' />
+
+                </HStack>
+            </Stack>
         </Box>
     )
 }
