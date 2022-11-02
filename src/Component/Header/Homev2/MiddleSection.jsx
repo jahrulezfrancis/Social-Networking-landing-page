8a9af7470d2df8bb9f76c36d129fc00daa50884b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-02 20:40:57 +0100

    updated middle section component

--- src/Component/Header/Homev2/MiddleSection.jsx
+++ src/Component/Header/Homev2/MiddleSection.jsx
@@ -1,10 +1,18 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { BiRightArrowAlt } from "react-icons/bi";
 
 export function MiddleSection() {
     return (
         <Box>
-            <Heading>Middle section component
+            <Heading bgColor='linearGradient(96.46deg, #C87C00 3.72%, #8B3208 31.05%, rgba(83, 32, 15, 0.82) 68.54%, #010420 100%)'>
+                Tell a better <br />
+                brand story
             </Heading>
+            <Text color='#425466'>
+                Automate the way you search through hashtags and suggested <br />
+                profiles to find results 100x faster.
+            </Text>
+            <Button rightIcon={<BiRightArrowAlt />} w='204px' borderRadius='24px' textAlign='center' fontSize='15px' bg='#231A36' color='white'>Request Demo</Button>
         </Box>
     )
 }
